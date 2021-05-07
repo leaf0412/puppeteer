487f90c6eddbf005b2cbc7f473f3a5eefa4f7a4e
sky-leaf <30424804+sky-leaf@users.noreply.github.com>
2021-05-07 23:49:43 +0800

    Update amz.js
    add async 多线程

--- amz.js
+++ amz.js
@@ -1,39 +1,41 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const chalk = require('chalk');
-const log = console.log;
-const red = chalk.red;
-const blue = chalk.blue;
-const orange = chalk.keyword('orange');
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+const _async = require('async')
+const chalk = require('chalk')
+const log = console.log
+const red = chalk.red
+const blue = chalk.blue
+const orange = chalk.keyword('orange')
+const { categoryTotalInfo } = require('./Home-Kitchen')
 
 async function spider(page) {
   return await page.evaluateOnNewDocument(() => {
-    () => {
+    ;() => {
       window.chrome = {
         app: {
           isInstalled: false,
           InstallState: {
             DISABLED: 'disabled',
             INSTALLED: 'installed',
-            NOT_INSTALLED: 'not_installed'
+            NOT_INSTALLED: 'not_installed',
           },
           RunningState: {
             CANNOT_RUN: 'cannot_run',
             READY_TO_RUN: 'ready_to_run',
-            RUNNING: 'running'
-          }
+            RUNNING: 'running',
+          },
         },
         runtime: {
           OnInstalledReason: {
             CHROME_UPDATE: 'chrome_update',
             INSTALL: 'install',
             SHARED_MODULE_UPDATE: 'shared_module_update',
-            UPDATE: 'update'
+            UPDATE: 'update',
           },
           OnRestartRequiredReason: {
             APP_UPDATE: 'app_update',
             OS_UPDATE: 'os_update',
-            PERIODIC: 'periodic'
+            PERIODIC: 'periodic',
           },
           PlatformArch: {
             ARM: 'arm',
@@ -41,14 +43,14 @@ async function spider(page) {
             MIPS: 'mips',
             MIPS64: 'mips64',
             X86_32: 'x86-32',
-            X86_64: 'x86-64'
+            X86_64: 'x86-64',
           },
           PlatformNaclArch: {
             ARM: 'arm',
             MIPS: 'mips',
             MIPS64: 'mips64',
             X86_32: 'x86-32',
-            X86_64: 'x86-64'
+            X86_64: 'x86-64',
           },
           PlatformOs: {
             ANDROID: 'android',
@@ -56,75 +58,94 @@ async function spider(page) {
             LINUX: 'linux',
             MAC: 'mac',
             OPENBSD: 'openbsd',
-            WIN: 'win'
+            WIN: 'win',
           },
           RequestUpdateCheckStatus: {
             NO_UPDATE: 'no_update',
             THROTTLED: 'throttled',
-            UPDATE_AVAILABLE: 'update_available'
-          }
-        }
-      };
-      const newProto = navigator.__proto__;
-      delete newProto.webdriver;
-      navigator.__proto__ = newProto;
-    };
-  });
+            UPDATE_AVAILABLE: 'update_available',
+          },
+        },
+      }
+      const newProto = navigator.__proto__
+      delete newProto.webdriver
+      navigator.__proto__ = newProto
+    }
+  })
 }
 
 function getRandomInt({ min = 0, max }) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 // 格式化时间
-function time(time = +new Date()) {
-  var date = new Date(time + 8 * 3600 * 1000);
-  return date.toJSON().substr(0, 19).replace('T', ' ').replace(/-/g, '.');
+Date.prototype.format = function (fmt) {
+  var o = {
+    'M+': this.getMonth() + 1, //月份
+    'd+': this.getDate(), //日
+    'h+': this.getHours(), //小时
+    'm+': this.getMinutes(), //分
+    's+': this.getSeconds(), //秒
+    'q+': Math.floor((this.getMonth() + 3) / 3), //季度
+    S: this.getMilliseconds(), //毫秒
+  }
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(
+      RegExp.$1,
+      (this.getFullYear() + '').substr(4 - RegExp.$1.length)
+    )
+  }
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length)
+      )
+    }
+  }
+  return fmt
 }
 
-//
+// 打乱数组顺序
 function shuffleSort(arr) {
-  var newArr = [...arr];
-  var n = newArr.length;
+  var newArr = [...arr]
+  var n = newArr.length
   while (n--) {
-    var index = Math.floor(Math.random() * n);
-    [newArr[index], newArr[n]] = [newArr[n], newArr[index]];
+    var index = Math.floor(Math.random() * n)
+    ;[newArr[index], newArr[n]] = [newArr[n], newArr[index]]
   }
-  return newArr;
+  return newArr
 }
 
 const browserOption = {
   headless: true,
   // 延迟
-  slowMo: 5
-};
+  slowMo: 5,
+}
 let userAgent = [
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
-];
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+]
 const chromeOption = {
   viewport: {
     width: 1280,
-    height: 800
+    height: 800,
   },
-  userAgent: userAgent[getRandomInt({ max: 1 })]
-};
+  userAgent: userAgent[getRandomInt({ max: 1 })],
+}
 const waitGotoUrlOption = {
   waitUntil: 'domcontentloaded',
-  timeout: 0
-};
-const url =
-  'https://www.amazon.com/Best-Sellers-Home-Kitchen/zgbs/home-garden/ref=zg_bs_nav_0';
+  timeout: 0,
+}
 
-let productDetailInfo = [];
-function getProductsInfo(page) {
-  return new Promise(async (resolve) => {
-    try {
-      const productInfo = await page.evaluate(() => {
-        let list = [];
-        document.querySelectorAll('.zg-item-immersion').forEach((item) => {
+async function getProductsInfo(page, productDetailInfo) {
+  try {
+    const productInfo = await page.evaluate(() => {
+      let list = []
+      document.querySelectorAll('.zg-item-immersion').forEach(item => {
+        if (item.querySelector('.a-link-normal')) {
           list.push({
             link: item.querySelector('.a-link-normal').href,
             stars: item.querySelector(
@@ -135,51 +156,41 @@ function getProductsInfo(page) {
             )?.innerText,
             picture: item.querySelector('.a-section.a-spacing-small img').src,
             price:
-              item.querySelector('.a-size-base.a-color-price')?.innerText || 0
-          });
-        });
-        return list;
-      });
-      log(red('揍你'));
-      productDetailInfo.push(...productInfo);
-      resolve();
-    } catch (err) {
-      red(err, 'error');
-    }
-  });
+              item.querySelector('.a-size-base.a-color-price')?.innerText || 0,
+          })
+        }
+      })
+      return list
+    })
+    productDetailInfo.push(...productInfo)
+  } catch (err) {
+    red(err, 'error')
+  }
 }
 
-function gotoNextPage(page) {
-  return new Promise(async (resolve) => {
-    try {
+async function gotoNextPage(page, productDetailInfo) {
+  try {
+    while (await page.$('.a-pagination .a-last a')) {
       const nextPage = await page.$eval(
         '.a-pagination .a-last a',
-        (item) => item.href
-      );
-      log(blue(nextPage), 'nextPage');
-      if (nextPage) {
-        await page.waitForTimeout(getRandomInt({ min: 2000, max: 2500 }));
-        await page.goto(nextPage, waitGotoUrlOption);
-        log(red('揍你'));
-        await getProductsInfo(page);
-        log(red('揍你'));
-        await gotoNextPage(page);
-      } else {
-        resolve();
-      }
-    } catch (err) {
-      red(err, 'error');
+        item => item.href
+      )
+      await page.goto(nextPage, waitGotoUrlOption)
+      await page.waitForTimeout(getRandomInt({ min: 1000, max: 2500 }))
+      await getProductsInfo(page, productDetailInfo)
     }
-  });
+  } catch (err) {
+    red(err, 'error')
+  }
 }
 
-async function run(url) {
-  const browser = await puppeteer.launch(browserOption);
+async function run(url, type, productDetailInfo) {
+  const browser = await puppeteer.launch(browserOption)
   try {
-    const page = await browser.newPage();
-    await spider(page);
-    await page.emulate(chromeOption);
-    await page.goto(url, waitGotoUrlOption);
+    const page = await browser.newPage()
+    await spider(page)
+    await page.emulate(chromeOption)
+    await page.goto(url, waitGotoUrlOption)
     // 设置  纽约
     // await page.waitForSelector(
     //   '.glow-toaster-button-submit .a-button-input[type="submit"]'
@@ -194,84 +205,106 @@ async function run(url) {
       // await page.keyboard.press('Enter')
       // await page.waitForTimeout(2000)
     }
-    await getProductsInfo(page);
-    await gotoNextPage(page);
-    log(blue('获取数据总条数', productDetailInfo.length));
-    // await saveProductInfo(page, shuffleSort(productDetailInfo));
+    await getProductsInfo(page, productDetailInfo)
+    await gotoNextPage(page, productDetailInfo)
+    log(blue(`${type} 获取数据总条数`, productDetailInfo.length))
+    await saveProductInfo(page, type, shuffleSort(productDetailInfo))
   } catch (err) {
-    log(red(err));
+    log(red(err))
   } finally {
-    await browser.close();
+    await browser.close()
   }
 }
 
 // 截取商品 id
 // /\/dp\/(.*?)\//i.exec(location.pathname)
 
-async function saveProductInfo(page, productDetailInfo) {
-  let productList = [];
-  for (let i = 0; i < productDetailInfo.length; i++) {
-    log(blue(`正在抓取第${i + 1}条商品数据...`));
+async function saveProductInfo(page, type, productDetailInfo) {
+  let productList = []
+  const productLength = productDetailInfo.length
+  for (let i = 0; i < productLength; i++) {
+    log(
+      blue(
+        `${type} 正在抓取第${i + 1}条商品数据... 进度 ${
+          ((i + 1) / productLength) * 100
+        }%`
+      )
+    )
     try {
-      const productUrl = productDetailInfo[i].link;
-      const productId = /\/dp\/(.*?)\//i.exec(productUrl);
-      await page.goto(productUrl, waitGotoUrlOption);
+      const productUrl = productDetailInfo[i].link
+      const productId = /\/dp\/(.*?)\//i.exec(productUrl)
+      await page.goto(productUrl, waitGotoUrlOption)
       const productInfo = await page.evaluate(async () => {
         const sleep = (time = 1000) => {
-          return new Promise((resolve) => {
-            setTimeout(() => resolve(), time);
-          });
-        };
+          return new Promise(resolve => {
+            setTimeout(() => resolve(), time)
+          })
+        }
         for (var y = 0; y <= 5; y++) {
-          window.scrollTo(0, 1280 * y);
-          await sleep();
+          window.scrollTo(0, 1280 * y)
+          await sleep()
         }
         var getKeys = {
           rank: '亚马逊热销商品排名',
-          available: 'Amazon.cn上架时间'
-        };
+          available: 'Amazon.cn上架时间',
+        }
         // var getKeys = {
         //   rank: 'Best Sellers Rank',
         //   available: 'Date First Available',
         // }
         let information = {
-          productTitle: document.querySelector('#productTitle').innerText
-        };
+          productTitle: document.querySelector('#productTitle')?.innerText,
+        }
         const productDetailTable = document.querySelectorAll(
           '.prodDetSectionEntry'
-        );
+        )
         const productDetailList = document.querySelectorAll(
           '.detail-bullet-list .a-list-item .a-text-bold'
-        );
+        )
         const productDetail =
-          productDetailTable.length > 0
-            ? productDetailTable
-            : productDetailList;
-        productDetail.forEach((item) => {
+          productDetailTable.length > 0 ? productDetailTable : productDetailList
+        productDetail.forEach(item => {
           for (let i in getKeys) {
             if (item.innerText.includes(getKeys[i])) {
-              information[i] = item.nextElementSibling.innerText;
+              information[i] = item.nextElementSibling.innerText
             }
           }
-        });
-        return information;
-      });
+        })
+        return information
+      })
       productList.push(
         Object.assign({}, productInfo, productDetailInfo[i], { productId })
-      );
-      await page.waitForTimeout(getRandomInt({ min: 2000, max: 2500 }));
+      )
+      await page.waitForTimeout(getRandomInt({ min: 1000, max: 2500 }))
     } catch (err) {
-      log(red(err));
-      continue;
+      log(red(err))
+      continue
     }
   }
-  await fs.writeFileSync(
-    `./${time()}-product.js`,
+  await fs.appendFileSync(
+    `./${new Date().format('yyyy-MM-dd')}-${type}-product.js`,
     JSON.stringify(productList),
     {
-      encoding: 'utf8'
+      encoding: 'utf8',
     }
-  );
+  )
 }
 
-run(url);
+const start = async () => {
+  log(orange(`start spider`))
+  _async.mapLimit(
+    categoryTotalInfo,
+    2,
+    (item, callback) => {
+      run(item.link, item.name, [], callback)
+    },
+    (err, res) => {
+      if (err) {
+        console.log('err======>', err)
+      }
+      console.log('==========>  ', res)
+    }
+  )
+}
+
+start()
